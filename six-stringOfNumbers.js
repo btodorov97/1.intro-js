ebdd75518fb07fe5490169a4a73d958d3a150392
btodorov97 <50707744+btodorov97@users.noreply.github.com>
2022-04-10 13:21:46 +0300

    Create six-stringOfNumbers.js

---
+++ six-stringOfNumbers.js
@@ -0,0 +1,9 @@
+function stringOfNumbers(lastNuumber) {
+    let result = '';
+    for (let i = 1; i <= lastNuumber; i++ ){
+        result+=i;
+    }
+    console.log(result); 
+}
+
+stringOfNumbers(10);
