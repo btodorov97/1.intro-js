d5a3caee9ba1d67706408d6a27bf59b11f14e81d
btodorov97 <50707744+btodorov97@users.noreply.github.com>
2022-04-11 01:54:33 +0300

    Update distanceBetween2Points.js

--- distanceBetween2Points.js
+++ distanceBetween2Points.js
@@ -1,11 +1,8 @@
 function distanceBetween2points (x1, x2, y1, y2) {
-    // let axisx = [x1,y1];
-    // let axisy = [x2,y2];
-    let y = x2 - x1;
-    let x = y2 - y1;
-//    let distance = Math.sqrt(Math.pow((x2 - x1),2) + Math.pow((y2 - y1),2));
-    let distance2 = Math.sqrt(x*x + y*y); 
-    return distance2;
+    let pointa = {x:parseInt(x1),y:parseInt(y1)};
+    let pointb = {x:parseInt(x2),y:parseInt(y2)};
+    let distance = Math.sqrt(Math.pow((pointb.x - pointa.x),2) + Math.pow((pointb.y - pointa.y),2));
+    return distance;
 }
 
 console.log(distanceBetween2points('1','2','3','4')); 
